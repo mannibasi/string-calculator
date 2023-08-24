c155f27cf45a10165e6f26141fe99efedde0a47b
Manni Basi <manni.basi@gmail.com>
2023-08-24 23:19:58 +0100

    Added simple implementation to handle two comma-separated nums

--- src/main/java/org/katas/StringCalculator.java
+++ src/main/java/org/katas/StringCalculator.java
@@ -1,10 +1,18 @@
 package org.katas;
 
+import java.lang.reflect.Array;
+
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(numbers);
+
+        if (numbers.length() == 1) {
+            return Integer.parseInt(numbers);
+        }
+
+        String[] numbersArray = numbers.split(",");
+        return Integer.parseInt(numbersArray[0]) + Integer.parseInt(numbersArray[1]);
     }
 }
