4d0c470719099f9d506c11e908e8a156dc0a502e
= <manni.basi@gmail.com>
2023-09-16 10:29:36 +0100

    Added failing test for newline delimiter

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -23,6 +23,12 @@ public class StringCalculatorShould {
     }
 
     @Test
+    public void return_3_when_input_is_1_2_with_new_line_separator() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1\n2"));
+    }
+
+    @Test
     public void return_45_when_input_is_1_2_3_4_5_6_7_8_9() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(45, stringCalculator.add("1,2,3,4,5,6,7,8,9"));
