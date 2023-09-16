a76728880ed7a3e906066b7353f90d1f0c7b2af0
= <manni.basi@gmail.com>
2023-09-16 10:35:05 +0100

    Added (already passing) test for mixed delimiters

--- src/test/java/StringCalculatorTests.java
+++ src/test/java/StringCalculatorTests.java
@@ -3,7 +3,7 @@ import org.katas.StringCalculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class StringCalculatorShould {
+public class StringCalculatorTests {
     @Test
     public void return_0_when_input_is_empty() {
         StringCalculator stringCalculator = new StringCalculator();
@@ -32,5 +32,11 @@ public class StringCalculatorShould {
     public void return_45_when_input_is_1_2_3_4_5_6_7_8_9() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(45, stringCalculator.add("1,2,3,4,5,6,7,8,9"));
+    }
+
+    @Test
+    public void return_6_when_input_is_1_2_3_with_mixed_delimiters() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(6, stringCalculator.add("1\n2,3"));
     }
 }
