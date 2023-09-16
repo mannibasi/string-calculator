1d2f82fb3fb1bab0f15286eb8c8cb7e42a276de9
= <manni.basi@gmail.com>
2023-09-16 10:32:35 +0100

    Updated regex for split to handle new line char / make test(s) pass

--- src/main/java/org/katas/StringCalculator.java
+++ src/main/java/org/katas/StringCalculator.java
@@ -12,7 +12,7 @@ public class StringCalculator {
             return Integer.parseInt(numbers);
         }
 
-        String[] numbersArray = numbers.split(",");
+        String[] numbersArray = numbers.split("[,\n]");
         int sum = 0;
         for (String number : numbersArray) {
             sum += Integer.parseInt(number);
